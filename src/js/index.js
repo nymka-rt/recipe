b7b252382b0ff8aaa3e33ad229c77e1d5450f840
Nyambayr <nymkanyambayar99@gmail.com>
2022-02-25 14:14:56 +0900

    First commit

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+const arr = [23, 44, 12];
+
+let myfunc = (a) => {
+  console.log("too : " + a);
+};
+
+const arr2 = [...arr, 44, 1223];
+myfunc(arr2);
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/js/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    static: "./dist",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "src/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
+      },
+    ],
+  },
+};
